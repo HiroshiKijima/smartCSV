f68b6d90f8d7987fdfd30d5705bf719032b11063
HiroshiKijima <>
2021-02-22 10:57:55 +0900

    csvの結果を読み取りスライスでまとめて返却する

--- smartCSV.go
+++ smartCSV.go
@@ -1,5 +1,27 @@
 package smartCSV
 
-func main() {
+import (
+	"encoding/csv"
+	"os"
+)
 
+func readFile(filepath string) (result [][]string, isErr bool) {
+	file, err := os.Open(filepath)
+	if err != nil {
+		return nil, true
+	}
+
+	defer file.Close()
+
+	csvReader := csv.NewReader(file)
+	var line []string
+	var output [][]string
+
+	for {
+		line, err = csvReader.Read()
+		if err != nil {
+			return output, false
+		}
+		output = append(output, line)
+	}
 }
